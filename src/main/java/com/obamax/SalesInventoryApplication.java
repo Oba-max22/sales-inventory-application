d9d2930e263590d0a609136ff9bc59d5a1d6d8a1
Emmanuel Macaulay <emmanuelmcl86@gmail.com>
2023-03-03 23:20:46 +0100

    created SalesInventoryApplication class

---
+++ src/main/java/com/obamax/SalesInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.obamax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesInventoryApplication.class, args);
+	}
+
+}
